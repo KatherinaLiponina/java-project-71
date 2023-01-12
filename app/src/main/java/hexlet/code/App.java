ea8b8b592944c165d93e44c7f944bcd58f666850
Katherine <liponina.katya@yandex.ru>
2023-01-12 12:33:56 +0300

    added option and parameters

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,15 +2,25 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+import java.io.File;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
          description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format", defaultValue="stylish")
+    private String format = "stylish";
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         if (args.length != 0) {
-            int exitCode = new CommandLine(new App()).execute(args);
-            System.out.println(exitCode);
+            new CommandLine(new App()).execute(args);
         }
     }
     
