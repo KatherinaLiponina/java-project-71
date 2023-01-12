a349cc09dc9a82e4a620e32bc1624b8e17e32402
Katherine <liponina.katya@yandex.ru>
2023-01-12 12:24:39 +0300

    added help and version

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,17 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+         description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        if (args.length != 0) {
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.out.println(exitCode);
+        }
     }
     
 }
